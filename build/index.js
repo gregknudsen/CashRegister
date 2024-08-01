7bfa399b8c46461583066b62cbf5267319bd4066
gregknudsen <dev.artist15@gmail.com>
2024-08-01 10:58:21 -0400

    simple console app complete, add new .md file to explain approach

--- build/index.js
+++ build/index.js
@@ -1,38 +1,83 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const readline = __importStar(require("readline"));
 const figlet_1 = __importDefault(require("figlet"));
+const chalk_1 = __importDefault(require("chalk"));
+const ChangeParser_1 = require("./ChangeParser");
 const CsvFileReader_1 = require("./CsvFileReader");
-const csvReader = new CsvFileReader_1.CsvFileReader("sample.csv");
-csvReader.read();
 const init = () => {
     intro();
 };
 const intro = () => {
-    console.log("Welcome to....");
+    console.log(chalk_1.default.blue("Welcome to...."));
     setTimeout(() => {
-        (0, figlet_1.default)("Change Parser!", (err, data) => {
+        (0, figlet_1.default)("Cash Register!", (err, data) => {
             if (err) {
                 console.log("Sorry, something went wrong");
                 console.dir(err);
                 return;
             }
-            console.log(data);
+            console.log(chalk_1.default.red(data));
+            setTimeout(dialog, 2000);
         });
     }, 2000);
 };
+const dialog = () => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question("Assuming you have a .csv file ready, are you ready to make change? [Y, n] ", answer => {
+        if (answer.toLocaleLowerCase() === "y") {
+            console.log("LET'S MAKE CHANGE!");
+            console.log(chalk_1.default.blue("-----------------------------------"));
+            parseData();
+        }
+        else {
+            console.log(chalk_1.default.red("See you next time!"));
+        }
+        rl.close();
+    });
+};
 init();
-// let results = csvReader.read()
-// csvReader.data.forEach((el: string[]) => {
-//   let amountOwed = Number(el[0]);
-//   let amountGiven = Number(el[1]);
-//   const changeParser = new ChangeParser(amountOwed, amountGiven);
-//   //  console.log(changeParser.difference);
-//   changeParser.provideChange();
-//   changeParser.summary();
-// });
+const parseData = () => {
+    const csvReader = new CsvFileReader_1.CsvFileReader("sample.csv");
+    csvReader.read();
+    csvReader.data.forEach((el) => {
+        let amountOwed = Number(el[0]);
+        let amountGiven = Number(el[1]);
+        const changeParser = new ChangeParser_1.ChangeParser(amountOwed, amountGiven);
+        //  console.log(changeParser.difference);
+        changeParser.provideChange();
+        changeParser.summary();
+    });
+};
 /*
 REQUIREMENTS
 
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,70 @@
+import * as readline from 'readline';
 import figlet from "figlet";
+import chalk from 'chalk';
 import { ChangeParser } from "./ChangeParser";
 import { CsvFileReader } from "./CsvFileReader";
 
 
-const csvReader = new CsvFileReader("sample.csv")
-csvReader.read();
 
-const init = () => {
+
+
+
+const init = (): void => {
   intro();
-  
 }
 
-
-const intro = () => {
-
-  console.log("Welcome to....")
+const intro = (): void => {
+  console.log(chalk.blue("Welcome to...."));
   
   setTimeout(() => {    
-    figlet("Change Parser!", (err, data) => {
+    figlet("Cash Register!", (err, data) => {
       if (err) {
         console.log("Sorry, something went wrong");
         console.dir(err);
         return;
       }
-      console.log(data);
+      console.log(chalk.red(data));
+      setTimeout(dialog,2000);
     });
   }, 2000)
 }
+const dialog = ():void => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  rl.question("Assuming you have a .csv file ready, are you ready to make change? [Y, n] ", answer => {
+    if (answer.toLocaleLowerCase() === "y") {
+      console.log("LET'S MAKE CHANGE!");
+      console.log(chalk.blue("-----------------------------------"));
+      parseData();
+    } else {
+      console.log(chalk.red("See you next time!"))
+    }
+    
+    rl.close();
+  })
+  
+}
+
 
 init();
 
+const parseData = (): void => {
+  const csvReader = new CsvFileReader("sample.csv")
+  csvReader.read();
 
-// let results = csvReader.read()
-// csvReader.data.forEach((el: string[]) => {
-//   let amountOwed = Number(el[0]);
-//   let amountGiven = Number(el[1]);
-
-//   const changeParser = new ChangeParser(amountOwed, amountGiven);
-//   //  console.log(changeParser.difference);
-//   changeParser.provideChange();
-//   changeParser.summary();
-// });
+  csvReader.data.forEach((el: string[]) => {
+    let amountOwed = Number(el[0]);
+    let amountGiven = Number(el[1]);
+    
+    const changeParser = new ChangeParser(amountOwed, amountGiven);
+    //  console.log(changeParser.difference);
+    changeParser.provideChange();
+    changeParser.summary();
+  });
+}
 
 
 /*
